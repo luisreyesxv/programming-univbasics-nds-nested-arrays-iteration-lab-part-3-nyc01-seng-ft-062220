e1ef396737bb90092ca9b240f1b6259d3cdd685c
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-11 01:48:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,10 +3,11 @@ def join_nested_strings(src)
   combined_strings = Array.new
 
   for x in src
-    if x.class == String
-      combined_strings << "#{x} "
-    end
-
+    for y in x
+      if y.class == String
+        combined_strings << "#{y} "
+      end
+    end  
 
   end
   binding.pry
